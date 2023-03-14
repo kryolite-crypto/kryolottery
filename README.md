c7ea6095f6acf42443602c3ae1d3715000f239af
NuclideK <88620225+NuclideK@users.noreply.github.com>
2023-03-14 12:36:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# KryoLottery viewer
+
+## Running in dev
+
+```bash
+  npm i
+  npm run dev
+```
---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+let url = "http://127.0.0.1:5000"
+let wallet = "kryo:5VXdN1K7VuSC8es21bbVpmRG4GPrgG5BsYyc"
+let response = "[loading]"
+
+async function run() {
+  fetch(`${url}/contract/${wallet}/call`, {
+      body: '{"method": "get_state"}',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+    }).then(response => response.text())
+    .then(text => {
+      console.log(text)
+      response = text
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
+run()
+</script>
+<body>
+    Response: {response}
+</body>
+
+<style>
+</style>
