0be4d828df48316ec87b234f4925c3969814903f
NuclideK <88620225+NuclideK@users.noreply.github.com>
2023-03-15 11:06:51 +0200

    Added an input field for lottery address

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,46 @@
 <script>
-let url = "http://127.0.0.1:5000"
-let wallet = "kryo:5VXdN1K7VuSC8es21bbVpmRG4GPrgG5BsYyc"
-let response = "[loading]"
+  import Bar from "./lib/bar.svelte";
+  let url = "http://127.0.0.1:5000"; // In production will either be testing testnet-1 to -3, but might also just be selectable
+  const transactionIDTEST = "kryo:5VXdN1K7VuSC8es21bbVpmRG4GPrgG5BsYyc"; // This is just a transaction on my local chain, easier to test if it's pre-set
+  let response = "";
 
-async function run() {
-  fetch(`${url}/contract/${wallet}/call`, {
+  async function addressInputted(event) {
+    let address = event.detail.data;
+    if (!address) return;
+    if (!address.startsWith("kryo:")) return;
+    if (address === "kryo:test") address = transactionIDTEST;
+
+    fetch(`${url}/contract/${address}/call`, {
       body: '{"method": "get_state"}',
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      method: "POST"
-    }).then(response => response.text())
-    .then(text => {
-      console.log(text)
-      response = text
+      method: "POST",
     })
-    .catch((error) => {
-      console.error(error)
-    })
-}
-run()
+      .then((response) => response.json())
+      .then((transactionObj) => {
+        console.log(transactionObj);
+        response = JSON.stringify(transactionObj);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 </script>
+
 <body>
+  <Bar
+    input_placeholder_text="Enter lottery address"
+    submit_button_text="Load"
+    on:inputSubmitted={addressInputted}
+  />
+  {#if response.length > 1}
     <p>Response: {response}</p>
+  {/if}
 </body>
 
 <style>
   p {
-    color: var(--color)
+    color: var(--color);
   }
 </style>
---
+++ src/lib/bar.svelte
@@ -0,0 +1,47 @@
+<script>
+  export let input_placeholder_text;
+  export let submit_button_text;
+  import { createEventDispatcher } from "svelte";
+  let input = "";
+
+  const dispatch = createEventDispatcher();
+
+  function inputSubmitted() {
+    dispatch("inputSubmitted", {
+      data: input,
+    });
+  }
+</script>
+
+<div id="barContainer">
+  <input bind:value={input} placeholder={input_placeholder_text} />
+  <button on:click={inputSubmitted}>{submit_button_text}</button>
+</div>
+
+<style>
+  #barContainer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  #barContainer * {
+    background-color: #252525;
+    border: solid 1px white;
+    color: aliceblue;
+  }
+
+  #barContainer input {
+    margin: 0px;
+    width: 90%;
+    height: 2rem;
+    font-size: 2rem;
+  }
+
+  #barContainer button {
+    margin: 0px;
+    width: 4rem;
+    height: 4rem;
+    font-size: 1rem;
+  }
+</style>
