bf1d0713102a5234e6bd246091cb12e8cbc203cb
NuclideK <88620225+NuclideK@users.noreply.github.com>
2023-03-17 13:30:16 +0200

    Registrants display properly

--- src/App.svelte
+++ src/App.svelte
@@ -2,19 +2,17 @@
   import Bar from "./lib/bar.svelte";
 
   const urlParams = new URLSearchParams(window.location.search);
-
-  if (urlParams.get("address") != undefined) {
-    addressInputted({ detail: { data: urlParams.get("address") } });
-  }
-
   const url = "http://127.0.0.1:5000"; // In production will either be testing testnet-1 to -3, but might also just be selectable
   const transactionIDTEST = "kryo:5VXdN1K7VuSC8es21bbVpmRG4GPrgG5BsYyc"; // This is just a transaction on my local chain, easier to test if it's pre-set
 
   let response = "";
   let contract;
 
-  let showingLastWinner = false;
-  let showingRegistrants = false;
+  let showingLastWinner = urlParams.get("showWin") === "true" || false;
+  let showingRegistrants = urlParams.get("showReg") === "true" || false;
+  if (urlParams.get("address") != undefined) {
+    addressInputted({ detail: { data: urlParams.get("address") } });
+  }
 
   async function addressInputted(event) {
     let address = event.detail.data;
@@ -33,9 +31,17 @@
       .then((contractObj) => {
         response = JSON.stringify(contractObj);
         contractObj["address"] = address;
+
+        if (address === transactionIDTEST) {
+          let list = [];
+          for (let i = 0; i < 32; i++) {
+            list.push(
+              `kryo:${(Math.random().toString(36) + "0").slice(2, 10 + 2)}`
+            );
+          }
+          contractObj["registrants"] = list;
+        }
         contract = contractObj;
-        showingLastWinner = false;
-        showingRegistrants = false;
         display();
       })
       .catch((error) => {
@@ -52,8 +58,12 @@
     // @ts-ignore
     registration_open.innerHTML = `registration_open : ${contract.registration_open}`;
     if (showingRegistrants) {
+      let text = "";
+      contract.registrants.forEach((address) => {
+        text += `${address}\n`;
+      });
       // @ts-ignore
-      registrants.innerHTML = `registrants : ${contract.registrants}`;
+      registrants.textContent = text;
     } else {
       // @ts-ignore
       registrants.innerHTML = `( Show registrants [${contract.registrants.length}] )`;
@@ -79,12 +89,20 @@
       id="lotteryDisplay"
       class="flex flex-col items-center bg-zinc-900 rounded-lg w-11/12"
     >
-      <p id="address" class="font-mono text-3xl m-2">[address]</p>
-      <p id="ticket_price" class="font-mono">[ticket_price]</p>
-      <p id="registration_open" class="font-mono">[registration_open]</p>
+      <p id="address" class="font-mono text-3xl m-2 text-slate-100">
+        [address]
+      </p>
+      <p id="ticket_price" class="font-mono text-slate-100">[ticket_price]</p>
+      <p id="registration_open" class="font-mono text-slate-100">
+        [registration_open]
+      </p>
       {#if showingRegistrants}
         <div id="registrants_container">
-          <p id="registrants" class="font-mono">[registrants]</p>
+          <textarea
+            class="bg-slate-600 h-96 w-64 font-mono text-slate-100 rounded-lg"
+            id="registrants"
+            name="registrants"
+          />
         </div>
       {:else}
         <button
@@ -96,12 +114,12 @@
           }}>( Show registrants [?] )</button
         >
       {/if}
-      {#if showingLastWinner === true}
+      {#if showingLastWinner}
         <div id="last_winner" class="flex flex-col items-center m-3">
-          <p id="last_winner_reward" class="font-mono text-xs">
+          <p id="last_winner_reward" class="font-mono text-xs text-slate-100">
             [last_winner_reward]
           </p>
-          <p id="last_winner_address" class="font-mono text-xs">
+          <p id="last_winner_address" class="font-mono text-xs text-slate-100">
             [last_winner_address]
           </p>
         </div>
@@ -110,7 +128,7 @@
           class="font-mono text-xs text-slate-500"
           on:click={() => {
             showingLastWinner = true;
-            setTimeout(display, 1);
+            setTimeout(display, 1); // Timeout so the DOM can update
           }}>(Show last winner)</button
         >
       {/if}
@@ -119,7 +137,4 @@
 </body>
 
 <style>
-  p {
-    color: var(--color);
-  }
 </style>
