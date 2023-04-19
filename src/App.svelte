6819286ade315e77f17b40fe34db9e8264f087ad
NuclideK <88620225+NuclideK@users.noreply.github.com>
2023-04-19 10:48:55 +0300

    Rewrite kryolottery viewer

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,6 @@
 
   const urlParams = new URLSearchParams(window.location.search);
   const url = "https://testnet-1.kryolite.io"; // TODO: Check multiple places
-  const transactionIDTEST = "kryo:null"; // Set this to something when doing local testing
 
   let response = "";
   let contract;
@@ -12,6 +11,11 @@
   let showingRegistrants = urlParams.get("showReg") === "true" || false;
   if (urlParams.get("address") != undefined) {
     addressInputted({ detail: { data: urlParams.get("address") } });
+  } else {
+    // By default give this address:
+    addressInputted({
+      detail: { data: "kryo:5VWWgFqhghrZvPmmXa48ajnd7vTawvYgbT4f" },
+    });
   }
 
   // Extracting this allows us to have multiple urls to fetch from later on
@@ -44,58 +48,17 @@
 
     if (!address) return;
     if (!address.startsWith("kryo:")) return; // All kryo addresses should start with this
-    if (address === "kryo:test") address = transactionIDTEST;
 
     post(url, `/contract/${address}/call`, { method: "get_state" }) // https://kryolite-crypto.github.io/api/
       .then((contractObj) => {
         response = JSON.stringify(contractObj);
         contractObj["address"] = address;
-
-        if (address === transactionIDTEST) {
-          let list = [];
-          list.push("kryo:5VWxQp616QM9k21RumcbKpA3kmAe2QoPDpdv");
-          for (let i = 0; i < 32; i++) {
-            list.push(
-              `kryo:${(Math.random().toString(36) + "0").slice(2, 10 + 2)}`
-            );
-          }
-          contractObj["registrants"] = list;
-        }
-
         contract = contractObj;
-        display();
+        console.log(contract);
       })
       .catch((error) => {
         console.error(error);
       });
-  }
-
-  async function display() {
-    if (!contract) return;
-    // @ts-ignore
-    address.innerHTML = `${contract.address}`;
-    // @ts-ignore
-    ticket_price.innerHTML = `ticket_price : ${contract.ticket_price}`;
-    // @ts-ignore
-    registration_open.innerHTML = `registration_open : ${contract.registration_open}`;
-    if (showingRegistrants) {
-      let text = "";
-      contract.registrants.forEach((address) => {
-        text += `${address}\n`;
-      });
-      // @ts-ignore
-      registrants.textContent = text;
-    } else {
-      // @ts-ignore
-      registrants.innerHTML = `( Show registrants [${contract.registrants.length}] )`;
-    }
-
-    if (showingLastWinner) {
-      // @ts-ignore
-      last_winner_reward.innerHTML = `last_winner_reward : ${contract.last_winner.reward}`;
-      // @ts-ignore
-      last_winner_address.innerHTML = `last_winner_address : ${contract.last_winner.address}`;
-    }
   }
 </script>
 
@@ -105,59 +68,57 @@
     submit_button_text="Load"
     on:inputSubmitted={addressInputted}
   />
-  <div class="flex flex-col items-center">
-    <div
-      id="lotteryDisplay"
-      class="flex flex-col items-center bg-zinc-900 rounded-lg w-11/12"
-    >
-      <p id="address" class="font-mono m-2 text-slate-100 scaletext">
-        [address]
-      </p>
-      <p id="ticket_price" class="font-mono text-slate-100 scaletext">
-        [ticket_price]
-      </p>
-      <p id="registration_open" class="font-mono text-slate-100 scaletext">
-        [registration_open]
-      </p>
-      {#if showingRegistrants}
+  {#if contract}
+    <div class="flex flex-col items-center">
+      <div
+        id="lotteryDisplay"
+        class="flex flex-col items-center bg-zinc-900 rounded-lg w-11/12"
+      >
+        <p class="font-mono m-2 text-slate-100 scaletext">
+          {contract.address}
+        </p>
+        <p class="font-mono text-slate-100 scaletext">
+          Entry cost: {contract.ticket_price}
+        </p>
+        {#if contract.registration_open}
+          <p class="font-mono text-slate-100 scaletext">
+            Registerations are open!
+          </p>
+        {:else}
+          <p class="font-mono text-slate-100 scaletext">
+            Registerations are closed!
+          </p>
+        {/if}
         <div id="registrants_container" class="flex flex-col items-center">
-          <textarea
-            disabled
+          <div
             class="bg-slate-600 font-mono w-full h-full text-slate-100 rounded-lg text-center"
             id="registrants"
-            name="registrants"
-          />
+          >
+            {#each contract.registrants as registerant}
+              <a
+                class="underline"
+                href="https://kryolite-crypto.github.io/explorer/wallet/?wallet={registerant}"
+                >{registerant}
+              </a>
+            {/each}
+          </div>
         </div>
-      {:else}
-        <button
-          class="font-mono text-xs text-slate-500"
-          id="registrants"
-          on:click={() => {
-            showingRegistrants = !showingRegistrants;
-            setTimeout(display, 1);
-          }}>( Show registrants [?] )</button
-        >
-      {/if}
-      {#if showingLastWinner}
         <div id="last_winner" class="flex flex-col items-center m-5">
+          <a
+            id="last_winner_address"
+            class="font-mono text-xs text-slate-100 underline"
+            href="https://kryolite-crypto.github.io/explorer/wallet/?wallet={contract
+              .last_winner.address}"
+          >
+            Last winner: {contract.last_winner.address}
+          </a>
           <p id="last_winner_reward" class="font-mono text-xs text-slate-100">
-            [last_winner_reward]
-          </p>
-          <p id="last_winner_address" class="font-mono text-xs text-slate-100">
-            [last_winner_address]
+            Last won prize: {contract.last_winner.reward}
           </p>
         </div>
-      {:else}
-        <button
-          class="font-mono text-xs text-slate-500"
-          on:click={() => {
-            showingLastWinner = true;
-            setTimeout(display, 1); // Timeout so the DOM can update
-          }}>(Show last winner)</button
-        >
-      {/if}
+      </div>
     </div>
-  </div>
+  {/if}
 </body>
 
 <style>
