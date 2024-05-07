99cd55aa96fefa31da7b0a42855b104a9cbf36b2
0xF00 <0xf00@kryolite.io>
2024-05-07 19:47:39 +0300

    Update address

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,26 @@
 <script>
   import Navbar from "./lib/navbar.svelte";
 
-  const contractAddress = "kryo:wea7jcaqpzm8vvdavbxqzv6zewue4f3u8y9wcz3492";
+  const contractAddress = "kryo:ag7td4m4c6ks7qhevxayamdjc5qbe5rcimmqyfqd";
   const url = "https://testnet-1.kryolite.io";
-  const KRYO_DIVISION = 1000000;
 
   let response = "";
   let contract;
+  let tickets = [];
 
-  // By default give this address:
-  addressInputted({
-    detail: { data: contractAddress },
-  });
+  const evtSource = new EventSource(url + `/contract/${contractAddress}/listen`);
 
-  // Extracting this allows us to have multiple urls to fetch from later on
-  // Such as testnet-1, -2, and -3
+  evtSource.onmessage = (event) => {
+    var ev = JSON.parse(event.data);
+    console.log(ev);
+
+    if (ev.Type === "Custom" && ev.Event.EventData[0] === "0") {
+      load();
+    }
+  };
+
+  load();
+
   async function post(url, path, body) {
     return new Promise((resolve, reject) => {
       fetch(`${url}${path}`, {
@@ -38,23 +44,47 @@
     });
   }
 
-  async function addressInputted(event) {
-    let address = event.detail.data;
-
-    if (!address) return;
-    if (!address.startsWith("kryo:")) return; // All kryo addresses should start with this
-
-    post(url, `/contract/${address}/call`, { method: "get_state" }) // https://kryolite-crypto.github.io/api/
+  async function load() {
+    post(url, `/contract/${contractAddress}/call`, { method: "GetState" })
       .then((contractObj) => {
         response = JSON.stringify(contractObj);
-        contractObj["address"] = address;
+        contractObj["address"] = contractAddress;
         contract = contractObj;
-        console.log(contract);
+
+        contract.tickets.forEach(owner => {
+          owner.tickets.forEach(ticket => {
+            tickets.push(
+              {
+                address: owner.address,
+                numbers: ticket.description.match(/\[(.*?)\]/)[1]
+              }
+            )
+          });
+        });
       })
       .catch((error) => {
         console.error(error);
       });
   }
+
+  const stringToHex = (str) => {
+    let hex = '';
+    for (let i = 0; i < str.length; i++) {
+      const charCode = str.charCodeAt(i);
+      const hexValue = charCode.toString(16);
+
+      // Pad with zeros to ensure two-digit representation
+      hex += hexValue.padStart(2, '0');
+    }
+    return hex;
+  };
+
+  export function updateParams()
+  {
+      return stringToHex(JSON.stringify({"method": "BuyTicket"}));
+  }
+
+  const params = updateParams();
 </script>
 
 <body>
@@ -64,35 +94,54 @@
     <div class="info">
       <p class="title">Info</p>
       <div class="container">
-          <div class="column">
+          <div class="column" style="width: 100%">
             <div class="row">
               <p class="header">Contract address</p>
               <p class="text">{contractAddress}</p>
             </div>
             <div class="row">
-              <p class="header">Ticket price</p>
-              <p class="text">{(contract.ticket_price / 1_000_000).toFixed(2)} kryo</p>
+              <p class="header">Ticket sale</p>
+              <p class="text">{contract.registrationOpen ? "Open" : "Closed"}</p>
             </div>
             <div class="row">
-              <p class="header">Ticket sale</p>
-              <p class="text">{contract.registration_open ? "Open" : "Closed"}</p>
+              <p class="header">Ticket price</p>
+              <p class="text">{(contract.ticketPrice / 1_000_000).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 6})} kryo</p>
             </div>
             <div class="row">
               <p class="header">Current pot</p>
-              <p class="text">{(contract.ticket_price * Object.keys(contract.tickets).length / 1_000_000 * 0.99).toFixed(2)} kryo</p>
+              <p class="text">{(contract.ticketPrice * tickets.length / 1_000_000 * 0.99).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 6})} kryo</p>
+            </div>
+            <div style="display: flex; justify-content: center">
+              <a class="btn-5" href="kryolite:call/{contractAddress}/{contract.ticketPrice}/{params}">BUY TICKET</a>
             </div>
           </div>
       </div>
-      <p class="title">Winner</p>
+      <p class="title">Last Draw</p>
       <div class="container">
-        <div class="column">
+        <div class="column" style="width: 100%">
           <div class="row">
-            <p class="header">Previous winner</p>
-            <p class="text">{contract.last_winner.address}</p>
+            <p class="header">Lucky Numbers</p>
+            <p class="text">{contract.previousDigits}</p>
           </div>
-          <div class="row">
-            <p class="header">Amount won</p>
-            <p class="text">{(contract.last_winner.reward / 1_000_000).toFixed(2)} kryo</p>
+          <div class="row" style="display:flex; justify-content: space-between">
+            <span>
+              <p class="header">Winners</p>
+              {#each contract.winners as {address}}
+              <div class="row">
+                <p class="text">
+                  <a href="https://kryolite-crypto.github.io/explorer/ledger/{address}">{address}</a>
+                </p>
+              </div>
+              {/each}
+            </span>
+            <span>
+              <p class="header">Reward</p>
+              {#each contract.winners as {reward}}
+              <div class="row">
+                <p class="text">{(reward / 1_000_000).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 6})} kryo</p>
+              </div>
+              {/each}
+            </span>
           </div>
         </div>
       </div>
@@ -102,12 +151,12 @@
       <div class="container fill-height">
           <div class="column">
             <div style="margin-bottom: 10px;">This is a demo application utilizing Kryolite Standard Token interface.</div>
-            <div>To participate in this lottery you need to buy a token using Kryolite Wallet.</div>
+            <!--div>To participate in this lottery you need to buy a token using Kryolite Wallet.</div>
             <ul>
               <li>Open wallet and go to Send tab</li>
               <li>Select wallet where to send funds from</li>
               <li>Copy {contractAddress} to recipient field, this is the contract address</li>
-              <li>Amount to send: {(contract.ticket_price / 1_000_000)}</li>
+              <li>Amount to send: {(contract.ticketPrice / 1_000_000)}</li>
               <li>From the next dropdown select <i>Buy ticket</i></li>
               <li>Click Send</li>
               <li>Wait for transaction to complete</li>
@@ -117,8 +166,8 @@
               Multiple tickets can be bought for same draw. If you don't see the dropdown which lets you select <i>Buy ticket</i> your walelt might not have fully synchronized yet.
             </div>
             <div>
-              After winner has been selected old tokens in your wallet will show up as consumed and they will not participate in future draws.
-            </div>
+              After winner has been drawn old tokens in your wallet will show up as consumed and they will not participate in future draws.
+            </div-->
           </div>
       </div>
     </div>
@@ -128,25 +177,17 @@
   <div class="container">
     <div class="column">
       <p class="header">Address</p>
-      {#each Object.entries(contract.address_to_tickets) as [address, tickets]}
+      {#each tickets as {address}}
       <p class="text">
-        <a href="https://kryolite-crypto.github.io/explorer/wallet/?wallet={address}">{address}</a>
+        <a href="https://kryolite-crypto.github.io/explorer/ledger/{address}">{address}</a>
       </p>
     {/each}
     </div>
     <div class="column">
-      <p class="header">Tickets bought</p>
-      {#each Object.entries(contract.address_to_tickets) as [address, tickets]}
+      <p class="header">Lotto numbers</p>
+      {#each tickets as {numbers}}
       <p class="text">
-        {tickets.length}
-      </p>
-    {/each}
-    </div>
-    <div class="column">
-      <p class="header">Chance to win</p>
-      {#each Object.entries(contract.address_to_tickets) as [address, tickets]}
-      <p class="text">
-        {(tickets.length / contract.tickets_sold * 100).toFixed(2)} %
+        {numbers}
       </p>
     {/each}
     </div>
